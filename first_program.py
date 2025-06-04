99d6db530cc9bc254294753945f5facbd13d8dd6
Prem Aryan <prem.aryan.1903@gmail.com>
2025-06-04 20:29:49 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -1,7 +1,6 @@
 #checkin number is of one digit or more than one digit
 
 number = int(input("Enter a number: "))
-number2 = int(input("Enterone more number: "))
 
 if number <= 9:
     print("one digit number.")
