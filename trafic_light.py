25485d5bd8eeca2a091cfe5032ae09fe3679167d
Prem Aryan <prem.aryan.1903@gmail.com>
2025-06-04 20:17:05 +0530

    Add files via upload

---
+++ trafic_light.py
@@ -0,0 +1,25 @@
+while "true":
+    que = input("red, yellow, green ;  choose any of ther colour: ")
+
+    if que == "red":
+        print("stop")
+    
+    elif que == "yellow":
+        print("ready to go")
+        
+    elif que == "green":
+        print("you can go now")
+        
+    else:
+        print("please enter the colour name from which is given")
+    
+
+    print("\n")
+   
+    print("\"enter exit to end here\"")
+
+    print("\n")
+        
+    if que == "exit":
+        break 
+    
