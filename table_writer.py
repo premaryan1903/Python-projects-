f8a5246edf42a23b637e2e52c56ef609eb98270a
Prem Aryan <prem.aryan.1903@gmail.com>
2025-08-01 21:29:26 +0530

    Update table_writer.py

--- table_writer.py
+++ table_writer.py
@@ -1,10 +1,16 @@
+a = "true"
+print("if you want to exit the program, enter 0.") 
+while "true":
+    num = int(input("enter number of which you want table: "))
 
+    i = 1
 
-num = int(input("enter number of which you want table: "))
+    while i <= 10:
+        print(num*i)
+        i += 1
 
-i = 1
+    if num == 0:
+        print("Exiting the program.")
+        break
 
-while i <= 10:
-    print(num*i)
-    i += 1
-    
+  
